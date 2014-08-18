9f69729c8697cb9f915e71ac3e244ba2ba5e68ea
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-18 21:28:17 +0100

    refactor into one page to do partials via angular

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -1,18 +1,87 @@
-@extends('layouts.default')
+<!DOCTYPE html>
+<html lang="en" ng-app="japanApp">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.22/angular.min.js"></script>
+  <script src="/js/controllers.js"></script>
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+</head>
+<body>
 
-@section('content')
-  <div class="jumbotron">
-    <h1>Cities of Japan</h1>
-    <p>Info about the cities will be here.. you can order them and stuff... yeah AngularJS is kind of cool like that...</p>
+  <nav class="navbar navbar-default" role="navigation">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Brand</a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Link</a></li>
+          <li><a href="#">Link</a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Action</a></li>
+              <li><a href="#">Another action</a></li>
+              <li><a href="#">Something else here</a></li>
+              <li class="divider"></li>
+              <li><a href="#">Separated link</a></li>
+              <li class="divider"></li>
+              <li><a href="#">One more separated link</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form class="navbar-form navbar-left" role="search">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="Search">
+          </div>
+          <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">Link</a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Action</a></li>
+              <li><a href="#">Another action</a></li>
+              <li><a href="#">Something else here</a></li>
+              <li class="divider"></li>
+              <li><a href="#">Separated link</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
+
+  <div class="container">
+    <div class="jumbotron">
+        <h1>Cities of Japan</h1>
+        <p>Info about the cities will be here.. you can order them and stuff... yeah AngularJS is kind of cool like that...</p>
+      </div>
+      <table class="table" ng-controller="CitiesCtrl as city">
+          <tr>
+              <th ng-click="predicate='name'">Name</th>
+              <th ng-click="predicate='population'">Population</th>
+          </tr>
+          <tr ng-repeat="city in cities | orderBy:predicate">
+              <td>@{{ city.name }}</td>
+              <td>@{{ city.population }}</td>
+          </tr>
+        </table>
   </div>
-  <table class="table" ng-controller="CitiesCtrl as city">
-      <tr>
-          <th ng-click="predicate='name'">Name</th>
-          <th ng-click="predicate='population'">Population</th>
-      </tr>
-      <tr ng-repeat="city in cities | orderBy:predicate">
-          <td>@{{ city.name }}</td>
-          <td>@{{ city.population }}</td>
-      </tr>
-    </table>
-@stop
+
+  <script src="//code.jquery.com/jquery.js"></script>
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+</body>
+</html>
