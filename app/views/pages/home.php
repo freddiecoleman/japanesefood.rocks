7aff48831e109d316715fafbe26e2516c166df4a
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-18 21:58:43 +0100

    view it up

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -68,19 +68,34 @@
         <h1>Cities of Japan</h1>
         <p>Info about the cities will be here.. you can order them and stuff... yeah AngularJS is kind of cool like that...</p>
       </div>
-      <input type="text" class="form-control" placeholder="Filter by name..." ng-model="cityName">
-      <table class="table" ng-controller="CitiesCtrl as city">
-          <tr>
-              <th>Pic</th>
-              <th ng-click="predicate='name'">Name</th>
-              <th ng-click="predicate='population'">Population</th>
-          </tr>
-          <tr ng-repeat="city in cities | orderBy:predicate | filter:cityName">
-              <td><img ng-src="/images/{{ city.thumbnail }}"></td>
-              <td>{{ city.name }}</td>
-              <td>{{ city.population | number }}</td>
-          </tr>
-        </table>
+      <div class="row">
+          <div class="col-md-12">
+              <input type="text" class="form-control" placeholder="Filter by name..." ng-model="cityName">
+          </div>
+      </div>
+      <div class="row">
+          <div class="col-md-6">
+              <h3>Cities</h3>
+              <table class="table" ng-controller="CitiesCtrl as city">
+                  <tr>
+                      <th>Pic</th>
+                      <th ng-click="predicate='name'">Name</th>
+                      <th ng-click="predicate='population'">Population</th>
+                  </tr>
+                  <tr ng-repeat="city in cities | orderBy:predicate | filter:cityName">
+                      <td><img ng-src="/images/{{ city.thumbnail }}"></td>
+                      <td>{{ city.name }}</td>
+                      <td>{{ city.population | number }}</td>
+                  </tr>
+              </table>
+          </div>
+          <div class="col-md-6">
+              <h3>Info</h3>
+
+          </div>
+      </div>
+
+
   </div>
 
   <script src="//code.jquery.com/jquery.js"></script>
