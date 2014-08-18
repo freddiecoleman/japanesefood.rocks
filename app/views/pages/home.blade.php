2da972501081247f972f17d0ea918f547c51dbd3
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-18 21:17:21 +0100

    list cities in table

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -1,11 +1,18 @@
 @extends('layouts.default')
 
 @section('content')
-  <div class="jumbotron" ng-controller="CitiesCtrl as city">
+  <div class="jumbotron">
     <h1>Welcome to my app!</h1>
     <p>test..1 + 1 = @{{ 1 + 1 }}</p>
-    <ul>
-        <li ng-repeat="city in cities">@{{ city.name }}</li>
-    </ul>
   </div>
+  <table class="table" ng-controller="CitiesCtrl as city">
+      <tr>
+          <th>Name</th>
+          <th>Population</th>
+      </tr>
+      <tr ng-repeat="city in cities">
+          <td>@{{ city.name }}</td>
+          <td>@{{ city.population }}</td>
+      </tr>
+    </table>
 @stop
