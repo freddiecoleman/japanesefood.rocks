58ff54fecdf00d9e19bd4e0cfd521ecb6ae2f6bd
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-21 21:17:05 +0100

    created city model and repository

---
+++ app/Japan/Cities/CityRepository.php
@@ -0,0 +1,16 @@
+<?php namespace Japan\Cities;
+
+
+class CityRepository {
+
+    public function save(City $city)
+    {
+        return $city->save();
+    }
+
+    public function all()
+    {
+        return City::all();
+    }
+
+}
