31f954a56358e533e00783d555838bba7d49c55f
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-22 16:55:08 +0100

    use view

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -52,7 +52,7 @@
     <!-- /.container-fluid -->
 </nav>
 
-
+<div ng-view></div>
 
 <script src="//code.jquery.com/jquery.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
