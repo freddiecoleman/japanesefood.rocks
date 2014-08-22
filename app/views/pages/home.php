5a708fc6c898451ff1c622688f0ddbb103cb1b13
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-22 21:06:13 +0100

    changed title of site to reflect the domain name I bought...

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -21,7 +21,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Japanese Food</a>
+            <a class="navbar-brand" href="#">Japanese Food Rocks!</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
