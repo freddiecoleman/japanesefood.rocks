b2ae0b96da769f0fd50f4cd9a72dc83506ee23c8
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-22 16:07:55 +0100

    tidy up

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -37,7 +37,6 @@
                       <button type="button" class="btn btn-primary btn-lg btn-block">Buy now!</button>
                   </div>
               </div>
-
           </div>
       </div>
   </div>
