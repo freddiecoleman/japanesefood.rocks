68f5b7a81cf66217ec2afe7503aaf20fbb3393ee
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-22 16:54:47 +0100

    extract to partial

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -51,47 +51,8 @@
     </div>
     <!-- /.container-fluid -->
 </nav>
-<div class="container">
-    <div class="page-header">
-        <h1>Ramen
-            <small>each city of Japan has its own unique flavour!</small>
-        </h1>
-    </div>
-    <div class="row">
-        <div class="col-md-6 city-list" ng-controller="RamenCtrl as flavour">
-            <div class="row">
-                <div class="col-md-3" ng-repeat="chosenRamen in ramen | limitTo: 12 | filter:searchBox"
-                     ng-click="select(chosenRamen)">
-                    <div class="thumbnail">
-                        <img ng-src="/images/thumbs/{{ chosenRamen.thumbnail }}" class="img-rounded">
 
-                        <div class="caption chosen ramen-thumb">
-                            <h1>{{ chosenRamen.name }}</h1>
 
-                            <p>
-                                <small>{{ chosenRamen.city.name }}</small>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6" ng-controller="SelectedCtrl as info">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <img data-ng-src="/images/pics/{{ selected.pic }}" ng-if="selected.pic"
-                         class="img-rounded full-pic">
-
-                    <h1>{{ selected.name }}
-                        <small>{{ selected.city.name }}</small>
-                    </h1>
-                    <p>{{ selected.description }}</p>
-                    <button type="button" class="btn btn-primary btn-lg btn-block">Buy now!</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 
 <script src="//code.jquery.com/jquery.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
