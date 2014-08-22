52753a9fc0334710a4a308f0be8fce157e8e3334
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-22 17:04:58 +0100

    add title

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -2,7 +2,7 @@
 <html lang="en" ng-app="japanApp">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Japanese Food</title>
     <script src="/js/lib/angular/angular.min.js"></script>
     <script src="/js/lib/angular-route/angular-route.min.js"></script>
     <script src="/js/controllers.js"></script>
