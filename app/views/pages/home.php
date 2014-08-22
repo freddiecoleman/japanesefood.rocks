471970b020d90c9c79ad12d9a4334b43cdb6e0bb
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-22 17:05:44 +0100

    add links to nav

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -27,8 +27,8 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Ramen</a></li>
-                <li><a href="#">Sushi</a></li>
+                <li class="active"><a href="/#/ramen">Ramen</a></li>
+                <li><a href="/#/sushi">Sushi</a></li>
                 <li><a href="#">Curry</a></li>
                 <li><a href="#">Snacks</a></li>
             </ul>
