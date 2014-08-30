2a4ec96bd97b7dff0508e9714b81e37610744dd9
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-30 18:08:59 +0100

    add intro to categories

--- app/database/migrations/2014_08_30_090035_create_categories_table.php
+++ app/database/migrations/2014_08_30_090035_create_categories_table.php
@@ -17,6 +17,7 @@ class CreateCategoriesTable extends Migration {
 			$table->increments('id');
 
             $table->string('name');
+            $table->mediumText('intro');
             $table->mediumText('description');
             $table->integer('parent_id');
             $table->string('thumbnail');
--- app/database/seeds/CategoriesTableSeeder.php
+++ app/database/seeds/CategoriesTableSeeder.php
@@ -35,6 +35,7 @@ class CategoriesTableSeeder extends Seeder {
 
         Category::create([
             'name'        => 'Ramen',
+            'intro'       => $faker->sentence(),
             'description' => 'Each region of Japan has it\'s own flavour of ramen. From the thick miso ramen of Hokkaido to the cloudy white tonkotsu ramen of Kyushuu.',
             'parent_id'   => Category::where('name', '=', 'Noodles')->firstOrFail()->id,
             'thumbnail'   => 'ramen.jpg',
@@ -43,6 +44,7 @@ class CategoriesTableSeeder extends Seeder {
 
         Category::create([
             'name'        => 'Udon',
+            'intro'       => $faker->sentence(),
             'description' => 'Udon can be served hot or cold and different toppings are used at different times of year. Like ramen there are different flavours of ramen from each region of Japan.',
             'parent_id'   => Category::where('name', '=', 'Noodles')->firstOrFail()->id,
             'thumbnail'   => 'udon.jpg',
@@ -51,6 +53,7 @@ class CategoriesTableSeeder extends Seeder {
 
         Category::create([
             'name'        => 'Sushi',
+            'intro'       => $faker->sentence(),
             'description' => 'The key to sushi is the vinegar flavoured rice. There are many different toppings; often raw seafood. Many types of sushi cannot be found outside of Japan.',
             'parent_id'   => Category::where('name', '=', 'Seafood')->firstOrFail()->id,
             'thumbnail'   => 'sushi.jpg',
@@ -59,6 +62,7 @@ class CategoriesTableSeeder extends Seeder {
 
         Category::create([
             'name'        => 'Sashimi',
+            'intro'       => $faker->sentence(),
             'description' => 'Le raw fish, unlike sushi it doesn\'t require rice.',
             'parent_id'   => Category::where('name', '=', 'Seafood')->firstOrFail()->id,
             'thumbnail'   => 'sashimi.jpg',
@@ -67,6 +71,7 @@ class CategoriesTableSeeder extends Seeder {
 
         Category::create([
             'name'        => 'Curry',
+            'intro'       => $faker->sentence(),
             'description' => 'sou desu ka',
             'parent_id'   => Category::where('name', '=', 'More food')->firstOrFail()->id,
             'thumbnail'   => 'curry.jpg',
@@ -75,6 +80,7 @@ class CategoriesTableSeeder extends Seeder {
 
         Category::create([
             'name'        => 'Okonomiyaki',
+            'intro'       => $faker->sentence(),
             'description' => 'sou desu ka',
             'parent_id'   => Category::where('name', '=', 'More food')->firstOrFail()->id,
             'thumbnail'   => 'okonomiyaki.jpg',
@@ -83,6 +89,7 @@ class CategoriesTableSeeder extends Seeder {
 
         Category::create([
             'name'        => 'Tonkatsu',
+            'intro'       => $faker->sentence(),
             'description' => 'sou desu ka',
             'parent_id'   => Category::where('name', '=', 'More food')->firstOrFail()->id,
             'thumbnail'   => 'tonkatsu.jpg',
@@ -91,6 +98,7 @@ class CategoriesTableSeeder extends Seeder {
 
         Category::create([
             'name'        => 'Gyoza',
+            'intro'       => $faker->sentence(),
             'description' => 'sou desu ka',
             'parent_id'   => Category::where('name', '=', 'More food')->firstOrFail()->id,
             'thumbnail'   => 'gyoza.jpg',
