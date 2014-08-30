37de2fc09320240d2d52f2124bca9d611eb2cc1f
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-30 12:48:15 +0100

    Create migration for food table

---
+++ app/database/migrations/2014_08_30_114655_create_food_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFoodTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('food', function(Blueprint $table)
+		{
+            $table->increments('id')->index();
+            $table->foreign('category_id')->references('id')->on('categories');
+
+            $table->string('name');
+            $table->string('name_jp');
+
+            $table->string('description');
+
+            $table->unsignedInteger('city_id');
+            $table->foreign('city_id')->references('id')->on('cities');
+
+            $table->string('thumbnail');
+            $table->string('pic');
+
+            $table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('food');
+	}
+
+}
