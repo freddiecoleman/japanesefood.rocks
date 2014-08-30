b2d22922e8411d53f3fa8e5699c38f085c6dd6cc
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-30 11:08:15 +0100

    created algorithm for displaying navigation in the correct order

--- app/Japan/Categories/CategoryRepository.php
+++ app/Japan/Categories/CategoryRepository.php
@@ -10,7 +10,47 @@ class CategoryRepository {
 
     public function orderedByParent()
     {
-        return Category::all();
+        $categories     = Category::all();
+        $rootCategories = [];
+        $subNav         = [];
+        $navigation     = [];
+
+        foreach ($categories as $category)
+        {
+
+            if ($category->parent_id == 0)
+            {
+                $rootCategories[] = ['id' => $category->id, 'name' => $category->name];
+            }
+
+        }
+
+        foreach ($rootCategories as $rootCategory)
+        {
+
+            foreach ($categories as $category)
+            {
+
+                if ($category->parent_id == $rootCategory['id'])
+                {
+
+                    $subNav[] = ['id' => $category->id, 'name' => $category->name];
+
+                }
+
+            }
+
+            $navigation[] = [
+                'root'   => $rootCategory['name'],
+                'subnav' => $subNav
+            ];
+
+            $subNav = [];
+
+        }
+
+
+        return $navigation;
     }
 
 }
